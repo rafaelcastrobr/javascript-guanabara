e544361277cc1ffa7ee1146f6287af933e039344
rafaelcastrobr <rafaelcastrobr@gmail.com>
2021-07-23 17:15:57 -0300

    updade program

--- Curso-js/ex017-analisador/script.js
+++ Curso-js/ex017-analisador/script.js
@@ -2,34 +2,56 @@ const btn = document.querySelector('#btn');
 let lista = [];
 
 btn.addEventListener('click', () => {
-  let num = document.querySelector('#num').value;
+  let num = document.querySelector('#num');
   let res = document.querySelector('#res');
-  let numero = Number(num);
+  let numero = Number(num.value);
 
-  if (num.length == 0) {
+
+  if (num.value.length == 0) {
     alert('Digite um número');
-  }
-  if(num >100) {
-    alert('Digite um número menor que 100')
+    return false;
+  } 
+
+  if (lista.indexOf(numero) !== -1) {
+    alert('Número já digitado, digite outro!');
+    return false;
+  } 
+
+  if(numero >100) {
+    alert('Digite um número menor que 100');
+  } else if (numero <= 0) {
+    alert('Digite um número maior que 0')
   } else {
     let op = document.createElement('option');
     res.appendChild(op);
-    op.text = `${num}`;
+    op.text = `Valor ${numero} adicionado.`;
     
     lista.push(numero);
   }
   resFinal.innerHTML = ``;
+  num.value = ``;
+  num.focus();
 
 });
 
 const btnRes = document.querySelector('#btnRes');
-
 
 btnRes.addEventListener('click', () => {
   let guarda = 0;
   let tamanhoL = lista.length -1;
   let cres = lista.sort();
 
+  let maior = lista[0];
+  let menor = lista[0];
+  
+  for (let pos in lista) {
+    if (lista[pos] > maior) {
+      maior = lista[pos];
+    }
+     if (lista[pos] < menor) {
+      menor = lista[pos];
+    }
+  }
 
   if (lista[0] == undefined){
     alert(`Adicione números antes de finalizar`)
@@ -37,12 +59,13 @@ btnRes.addEventListener('click', () => {
     for(let i=0; i<=tamanhoL ;i++){
       guarda += lista[i];
     }
+
     const resFinal = document.querySelector('#resFinal');
     resFinal.innerHTML = `<p>Ao todo, temos ${lista.length} números cadastrados</p>`
-    resFinal.innerHTML += `<p>O maior valor informado foi ${cres[tamanhoL]}</p>`
-    resFinal.innerHTML += `<p>O menor valor informado foi ${cres[0]}</p>`
+    resFinal.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+    resFinal.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
     resFinal.innerHTML += `<p>Somando todos os valores temos ${guarda}</p>`
     resFinal.innerHTML += `<p>A média dos valores digitados é ${guarda/lista.length}</p>`
   }
-  
+
 });
--- Curso-js/ex017-analisador/test.js
+++ Curso-js/ex017-analisador/test.js
@@ -1,6 +1,8 @@
-let lista = [];
+let num = document.querySelector('#num');
+let lista = document.querySelector('#res');
+let res = document.querySelector('#resFinal');
+let valores = [];
 
-lista.push(4);
-lista.push(5);
+function adicionar() {
 
-console.log(lista)
+}
